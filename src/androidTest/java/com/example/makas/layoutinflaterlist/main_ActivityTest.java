6ab73c0628fc657efde3d4f51cdd2ffcad8c1d5c
makasun <makasun61@gmail.com>
2015-12-21 22:17:48 +0200

    ScrollView
    ScrollView

---
+++ src/androidTest/java/com/example/makas/layoutinflaterlist/main_ActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.makas.layoutinflaterlist;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by makas on 20.12.2015.
+ */
+public class main_ActivityTest extends TestCase {
+
+}
